5ac572edcb4fe391e655e99f8231a57f8b5ddd9b
0andme <yu00mi97@gmail.com>
2023-02-09 13:35:53 +0900

    2. 할일 등록 기능

--- my-todo1/src/components/TodoMain.vue
+++ my-todo1/src/components/TodoMain.vue
@@ -4,8 +4,8 @@
     <main>
       <div class="todos">
         <div class="write">
-          <input type="text" />
-          <button class="btn add">Add</button>
+          <input type="text" v-model="addItemText" @keyup.enter="addItem" />
+          <button class="btn add" @click="addItem">Add</button>
         </div>
         <ul class="list">
           <li v-for="todo in todos" :key="todo.text">
@@ -33,12 +33,22 @@
 export default {
   data() {
     return {
+      addItemText: "",
       todos: [
         {text: "공부하기", state: "yet"},
         {text: "운동하기", state: "done"},
         {text: "글쓰기", state: "done"},
       ],
     };
+  },
+  methods: {
+    addItem() {
+      this.todos.push({
+        text: this.addItemText,
+        state: "yet",
+      });
+      this.addItemText = "";
+    },
   },
 };
 </script>
