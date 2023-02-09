0b39f9ca4bcf7860a82a15c8d2028373c2a12938
0andme <yu00mi97@gmail.com>
2023-02-09 11:25:36 +0900

    #1 conf : 프로젝트 설정
    - vue 프로젝트 시작
    - font awesome link
    - web font import
    - reset css 추가

---
+++ my-todo1/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app"><todo-main></todo-main></div>
+</template>
+
+<script>
+import TodoMain from "./components/TodoMain.vue";
+export default {
+  components: {TodoMain},
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Merriweather:wght@900&family=Noto+Sans+KR:wght@100;400&display=swap");
+/* reset */
+* {
+  margin: 0;
+  padding: 0;
+}
+html {
+  font-size: 16px;
+}
+body {
+  font-family: "Noto Sans KR", sans-serif;
+}
+li {
+  list-style: none;
+}
+a {
+  text-decoration-line: none;
+  color: black;
+}
+input,
+button,
+a {
+  outline: none;
+}
+</style>
